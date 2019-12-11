feef225d4594a962a9b9e93aa7d6c0e7603d2a64
Nicolas CHERRIDI <nicolas.cherridi@revevol.eu>
2019-12-11 19:00:32 +0100

    first commit

---
+++ src/main/java/wtf/nicoto/Voteit/VoteItApplication.java
@@ -0,0 +1,13 @@
+package wtf.nicoto.Voteit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wtf/nicoto/Voteit/VoteItApplicationTests.java
@@ -0,0 +1,13 @@
+package wtf.nicoto.Voteit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
